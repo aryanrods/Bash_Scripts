cf6db854aa378ea4115b74b5ece38e9ac3cd5db4
aryanrods <rodriguesaryan00@gmai.com>
2025-07-16 18:25:21 +1000

    basics3.sh done

--- Basics/basics3.sh
+++ Basics/basics3.sh
@@ -1,7 +1,12 @@
-#!/bin/bash 
+#!/bin/bash
 
-echo "What's your name?" 
+echo "Please enter a number: "
+read num
 
-read entered_name 
-
-echo -e "\nWelcome to bash tutorial" $entered_name
+if [ $num -gt 0 ]; then
+  echo "$num is positive"
+elif [ $num -lt 0 ]; then
+  echo "$num is negative"
+else
+  echo "$num is zero"
+fi
